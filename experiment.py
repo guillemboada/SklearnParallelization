5b25ffab866014b92ee37cb57402501fd50b79fb
guillemboada <guillemboada@hotmail.com>
2023-03-12 18:42:28 +0100

    Rename experiment script

--- experiment.py
+++ experiment.py
@@ -26,7 +26,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="{asctime} {levelname:<8} {message}",
     style='{',
-    filename='performance.log'
+    filename='experiment.log'
 )
 
 # Read experiment configuration variables
